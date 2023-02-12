27a97be302ec5f1c08fcdeae2d0daa49e360eb40
JoelKanyi <joelkanyi98@gmail.com>
2023-02-12 09:24:26 +0300

    something usable - 01

--- app/src/main/java/com/joelkanyi/composesignature/MainActivity.kt
+++ app/src/main/java/com/joelkanyi/composesignature/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.joelkanyi.composesignature
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.joelkanyi.composesignature.ui.theme.ComposeSignatureTheme
+import com.joelkanyi.composesignature.ui.theme.Pink40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +47,37 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.White
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeSignature()
+                    var imageBitmap: ImageBitmap? by remember {
+                        mutableStateOf(null)
+                    }
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        ComposeSignature(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            signaturePadColor = Color(0xFFEEEEEE),
+                            signaturePadHeight = 400.dp,
+                            signatureColor = Color.Black,
+                            signatureThickness = 10f,
+                            onComplete = { signatureBitmap ->
+                                imageBitmap = signatureBitmap.asImageBitmap()
+                            }
+                        )
+
+                        Spacer(modifier = Modifier.height(24.dp))
+
+                        if (imageBitmap != null) {
+                            Image(
+                                modifier = Modifier
+                                    .padding(12.dp)
+                                    .size(200.dp),
+                                bitmap = imageBitmap!!,
+                                contentDescription = null
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -34,6 +88,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposeSignatureTheme {
-        ComposeSignature()
+        Column(Modifier.fillMaxSize()) {
+            ComposeSignature(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp),
+                signaturePadColor = Color(0xFFEEEEEE),
+                signaturePadHeight = 500.dp,
+                signatureColor = Color.Black,
+                signatureThickness = 10f,
+                onComplete = {
+
+                }
+            )
+        }
     }
 }
--- composesignature/src/main/java/com/joelkanyi/composesignature/ComposeSignature.kt
+++ composesignature/src/main/java/com/joelkanyi/composesignature/ComposeSignature.kt
@@ -1,5 +1,6 @@
 package com.joelkanyi.composesignature
 
+import android.graphics.Bitmap
 import android.view.MotionEvent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -30,53 +32,58 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.drawToBitmap
+import java.util.*
 
 @Composable
 fun ComposeSignature(
     modifier: Modifier = Modifier,
+    signaturePadColor: Color = Color(0xFFEEEEEE),
     signaturePadHeight: Dp = 500.dp,
+    signatureColor: Color = Color.Black,
+    signatureThickness: Float = 10f,
+    onComplete: (Bitmap) -> Unit,
+    onClear:() -> Unit = {}
 ) {
-    val viewModel: SignaturePadViewModel = viewModel()
+    // val viewModel: SignaturePadViewModel = viewModel()
     val path = remember { mutableStateOf(mutableListOf<PathState>()) }
-    val drawColor = remember { mutableStateOf(Color.Black) }
-    val drawBrush = remember { mutableStateOf(8f) }
+    val drawColor = remember { mutableStateOf(signatureColor) }
+    val drawBrush = remember { mutableStateOf(signatureThickness) }
 
 
     Card(
         modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(16.dp),
+            .wrapContentWidth()
+            .wrapContentHeight(),
         shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0xFFEEEEEE)),
+        colors = CardDefaults.cardColors(containerColor = signaturePadColor),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 4.dp
         )
     ) {
         Column {
-
-
             path.value.add(PathState(Path(), drawColor.value, drawBrush.value))
 
-            DrawingCanvas(
-                drawColor = drawColor,
-                drawBrush = drawBrush,
-                path = path.value,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(signaturePadHeight)
-            )
+            val signatureBitmap = captureBitmap {
+                DrawingCanvas(
+                    drawColor = drawColor,
+                    drawBrush = drawBrush,
+                    path = path.value,
+                    modifier = modifier.height(signaturePadHeight)
+                )
+            }
 
             Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
+                modifier = modifier
+                    .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -92,15 +99,12 @@ fun ComposeSignature(
                     }
                 )
                 ButtonComponent(
-                    title = "Complete",
-                    icon = R.drawable.ic_sign,
+                    title = "Done",
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(48.dp)
                         .padding(start = 4.dp),
-                    onClick = {
-
-                    }
+                    onClick = { onComplete(signatureBitmap.invoke()) }
                 )
             }
         }
@@ -119,39 +123,40 @@ fun DrawingCanvas(
     val currentPath = path.last().path
     val movePath = remember { mutableStateOf<Offset?>(null) }
 
-    Canvas(
-        modifier = modifier
-            .pointerInteropFilter {
-                when (it.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        currentPath.moveTo(it.x, it.y)
+        Canvas(
+            modifier = modifier
+                .pointerInteropFilter {
+                    when (it.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            currentPath.moveTo(it.x, it.y)
+                        }
+                        MotionEvent.ACTION_MOVE -> {
+                            movePath.value = Offset(it.x, it.y)
+                        }
+                        else -> {
+                            movePath.value = null
+                        }
                     }
-                    MotionEvent.ACTION_MOVE -> {
-                        movePath.value = Offset(it.x, it.y)
-                    }
-                    else -> {
-                        movePath.value = null
-                    }
+                    true
                 }
-                true
+        ) {
+            movePath.value?.let {
+                currentPath.lineTo(it.x, it.y)
+                drawPath(
+                    path = currentPath,
+                    color = drawColor.value,
+                    style = Stroke(drawBrush.value)
+                )
             }
-    ) {
-        movePath.value?.let {
-            currentPath.lineTo(it.x, it.y)
-            drawPath(
-                path = currentPath,
-                color = drawColor.value,
-                style = Stroke(drawBrush.value)
-            )
+            path.forEach {
+                drawPath(
+                    path = it.path,
+                    color = it.color,
+                    style = Stroke(it.stroke)
+                )
+            }
         }
-        path.forEach {
-            drawPath(
-                path = it.path,
-                color = it.color,
-                style = Stroke(it.stroke)
-            )
-        }
-    }
+
 }
 
 @Composable
@@ -159,7 +164,7 @@ fun ButtonComponent(
     title: String,
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
-    icon: Int,
+    icon: Int? = null,
 ) {
     Box(
         modifier = modifier
@@ -176,11 +181,13 @@ fun ButtonComponent(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(6.dp),
         ) {
-            Icon(
-                painter = painterResource(id = icon),
-                tint = Color.White,
-                contentDescription = null
-            )
+            if (icon != null) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
             Text(
                 text = title,
                 color = Color.White,
@@ -189,4 +196,40 @@ fun ButtonComponent(
             )
         }
     }
+}
+
+
+@Composable
+fun captureBitmap(
+    content: @Composable () -> Unit,
+): () -> Bitmap {
+
+    val context = LocalContext.current
+
+    /**
+     * ComposeView that would take composable as its content
+     * Kept in remember so recomposition doesn't re-initialize it
+     **/
+    val composeView = remember { ComposeView(context) }
+
+    /**
+     * Callback function which could get latest image bitmap
+     **/
+    fun captureBitmap(): Bitmap {
+        return composeView.drawToBitmap()
+    }
+
+    /** Use Native View inside Composable **/
+    AndroidView(
+        factory = {
+            composeView.apply {
+                setContent {
+                    content.invoke()
+                }
+            }
+        }
+    )
+
+    /** returning callback to bitmap **/
+    return ::captureBitmap
 }
