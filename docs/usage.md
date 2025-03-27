485200e94dd940445f44f34e5285d17f70c58f43
Joel Kanyi <joelkanyi98@gmail.com>
2025-03-27 06:52:45 +0300

    feat: return old Sain but mark it as deprecated

--- docs/usage.md
+++ docs/usage.md
@@ -1,20 +1,20 @@
 Add the `Sain` composable into your project and customize it according to your needs:
+
+#### Usage
+Add the `Sain` composable into your project and customize it according to your needs:
 ```kotlin
 var imageBitmap: ImageBitmap? by remember {
     mutableStateOf(null)
 }
 
 Sain(
-    modifier = Modifier
-        .fillMaxWidth()
-        .height(250.dp)
-        .border(
-            BorderStroke(
-                width = .5.dp,
-                color = MaterialTheme.colorScheme.onSurface
-            ),
-            shape = RoundedCornerShape(8.dp)
-        ),
+    signatureHeight = 250.dp,
+    signaturePadColor = Color.White,
+    signatureBorderStroke = BorderStroke(
+        width = .5.dp,
+        color = MaterialTheme.colorScheme.onSurface,
+    ),
+    signaturePadShape = RoundedCornerShape(8.dp),
     onComplete = { signatureBitmap ->
         if (signatureBitmap != null) {
             imageBitmap = signatureBitmap
@@ -27,26 +27,30 @@ Sain(
         modifier = Modifier
             .padding(top = 16.dp)
             .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         Button(
             modifier = Modifier.weight(1f),
             onClick = {
                 imageBitmap = null
                 action(SignatureAction.CLEAR)
-            }) {
+            },
+        ) {
             Text("Clear")
         }
         Button(
             modifier = Modifier.weight(1f),
             onClick = {
                 action(SignatureAction.COMPLETE)
-            }) {
+            },
+        ) {
             Text("Complete")
         }
     }
 }
 ```
+
+</br>
 
 #### Actions
 The `Sain` composable takes in an `actions` parameter which is a lambda that takes in a `SignatureAction` enum. The `SignatureAction` enum has two values:
@@ -58,11 +62,8 @@ The `Sain` composable takes in an `actions` parameter which is a lambda that tak
 #### Customization
 - `signatureColor` - The color of the signature
 - `signatureThickness` - The thickness of the signature
-- `modifier` - The modifier for the `Sain` composable which allows you to customize the size, shape, background, border etc of the signature view
-
-</br>
-
-#### Known Issues
-The library works well in one orientation. If you rotate the device, the signature will be lost. This is a known issue and will be fixed in the next release.
-
-> Note: The library is now hosted on Maven Central. If you were using the previous version hosted on Jitpack, please update your dependencies to the latest version.
+- `signatureHeight`: The height of the signature pad.
+- `signaturePadColor`: The color of the signature pad.
+- `signatureBorderStroke`: The border stroke of the signature pad.
+- `signaturePadShape`: The shape of the signature pad.
+  </br>
--- sample/shared/src/commonMain/kotlin/com.composesignature.sample/Sample.kt
+++ sample/shared/src/commonMain/kotlin/com.composesignature.sample/Sample.kt
@@ -15,6 +15,7 @@
  */
 package com.composesignature.sample
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
@@ -28,10 +29,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -45,80 +53,105 @@ import androidx.compose.ui.unit.dp
 import io.github.joelkanyi.sain.Sain
 import io.github.joelkanyi.sain.SignatureAction
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sample(
     modifier: Modifier = Modifier,
 ) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background,
-    ) {
-        var imageBitmap by remember {
-            mutableStateOf<ImageBitmap?>(null)
-        }
-
-        Column(
-            modifier = Modifier
-                .scrollable(rememberScrollState(), orientation = Orientation.Horizontal)
-                .padding(16.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "By signing you agree to our terms and conditions. Please sign below.",
-                style = MaterialTheme.typography.bodyMedium,
-            )
-
-            Sain(
-                signatureHeight = 250.dp,
-                signaturePadColor = Color.White,
-                signaturePadBorderColor = MaterialTheme.colorScheme.onSurface,
-                signaturePadBorderThickness = .5.dp,
-                signaturePadShape = RoundedCornerShape(8.dp),
-                onComplete = { signatureBitmap ->
-                    if (signatureBitmap != null) {
-                        imageBitmap = signatureBitmap
-                    } else {
-                        println("Signature is empty")
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            // Handle navigation back
+                        },
+                    ) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 },
-            ) { action ->
-                Row(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = {
-                            imageBitmap = null
-                            action(SignatureAction.CLEAR)
-                        },
-                    ) {
-                        Text("Clear")
-                    }
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = {
-                            action(SignatureAction.COMPLETE)
-                        },
-                    ) {
-                        Text("Complete")
-                    }
-                }
+                title = {
+                    Text("Compose Signature")
+                },
+            )
+        },
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            var imageBitmap by remember {
+                mutableStateOf<ImageBitmap?>(null)
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            imageBitmap?.let {
-                Image(
-                    modifier = Modifier,
-                    bitmap = imageBitmap!!,
-                    contentDescription = null,
+            Column(
+                modifier = Modifier
+                    .scrollable(rememberScrollState(), orientation = Orientation.Horizontal)
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "By signing you agree to our terms and conditions. Please sign below.",
+                    style = MaterialTheme.typography.bodyMedium,
                 )
+
+                Sain(
+                    signatureHeight = 250.dp,
+                    signaturePadColor = Color.White,
+                    signatureBorderStroke = BorderStroke(
+                        width = .5.dp,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    ),
+                    signaturePadShape = RoundedCornerShape(8.dp),
+                    onComplete = { signatureBitmap ->
+                        if (signatureBitmap != null) {
+                            imageBitmap = signatureBitmap
+                        } else {
+                            println("Signature is empty")
+                        }
+                    },
+                ) { action ->
+                    Row(
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    ) {
+                        Button(
+                            modifier = Modifier.weight(1f),
+                            onClick = {
+                                imageBitmap = null
+                                action(SignatureAction.CLEAR)
+                            },
+                        ) {
+                            Text("Clear")
+                        }
+                        Button(
+                            modifier = Modifier.weight(1f),
+                            onClick = {
+                                action(SignatureAction.COMPLETE)
+                            },
+                        ) {
+                            Text("Complete")
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                imageBitmap?.let {
+                    Image(
+                        modifier = Modifier,
+                        bitmap = imageBitmap!!,
+                        contentDescription = null,
+                    )
+                }
             }
         }
     }
