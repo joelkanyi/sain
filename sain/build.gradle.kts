2376c42c7fa03c01131971b8d72ed718a00fdec5
Joel Kanyi <joelkanyi98@gmail.com>
2025-07-07 07:43:58 +0300

    chore: update nmcp plugin to version 1.0.1 and adjust configuration

--- sain/build.gradle.kts
+++ sain/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compatibility)
     alias(libs.plugins.dokka)
-    alias(libs.plugins.nmcp)
+    alias(libs.plugins.nmcp.aggregation)
     alias(libs.plugins.gradleMavenPublish)
     alias(libs.plugins.compose.compiler)
 }
@@ -124,7 +124,7 @@ mavenPublishing {
     }
 }
 
-nmcp {
+nmcpAggregation {
     centralPortal {
         username = System.getenv("MAVEN_CENTRAL_USERNAME")
         password = System.getenv("MAVEN_CENTRAL_PASSWORD")
